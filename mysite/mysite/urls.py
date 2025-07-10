25dd9763e48a4122f5c48cb50c1770255f75ca4b
Tony Ji <tonyji@outlook.com>
2025-07-10 20:44:53 +0800

    update urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myapp.views import listorders
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('myapp/orders/', listorders),
 ]
